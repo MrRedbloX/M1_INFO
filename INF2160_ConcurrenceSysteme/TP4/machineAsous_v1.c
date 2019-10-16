29fa6e83d83b34439d7b45973fff0a2b58567001
MrRedbloX <charles.cogoluegnes@gmail.com>
2019-10-16 18:41:35 +0200

    INF2160_ConcurrenceSysteme TP4 MODIFY

--- INF2160_ConcurrenceSysteme/TP4/machineAsous_v1.c
+++ INF2160_ConcurrenceSysteme/TP4/machineAsous_v1.c
@@ -11,9 +11,11 @@
 #include <signal.h>
 
 void handler(int sig){
-	usleep(5000);
-	exit(1);
-
+	if(sig == SIGUSR1){		
+		fdatasync(0);
+		//printf("Exit %d\n", getpid());
+		exit(1);
+	}
 } 
 
 int main (int argc, char ** argv) {
@@ -25,6 +27,7 @@ int main (int argc, char ** argv) {
 
   int semid;
   int shmid;
+  int tabPid[4096];
 
   ushort init_sem[1]={1};
   //Mutex
@@ -55,23 +58,23 @@ int main (int argc, char ** argv) {
    int i;
    time_t t;
    pid_t pid;
+   sigset_t set;
+   struct sigaction actions;
+   int rc;
+   
    srandom(time(&t));
 
    for(i=0; i<atoi(argv[1]); i++) *(sem+i) = random()%10;
-   shmdt(sem);
    struct sembuf op;
    for(i=0; i<atoi(argv[1]); i++){
-	   if((pid = fork() == 0)){
-		   
-		   struct sigaction actions;
-		   int rc;
-		   sigset_t set;
-		   
+	   pid = fork();
+	   *(tabPid+i) = pid;
+	   if(pid == 0){
+		   //printf("PID f : %d\n",getpid());
 		   sigemptyset(&actions.sa_mask);
 		   actions.sa_flags = 0;
 		   actions.sa_handler = handler;
 		   rc = sigaction(SIGUSR1,&actions,NULL);
-		     
 		   while(1){
 			   sigemptyset(&set);
 			   sigaddset(&set,SIGUSR1);
@@ -88,8 +91,7 @@ int main (int argc, char ** argv) {
 			   if(*(sem+i) == 9) *(sem+i) = 0;
 			   else *(sem+i) = *(sem+i) + 1;
 			   
-			   shmdt(sem);
-			   usleep(50);		   
+			   usleep(100);		   
 			   op.sem_num=0;op.sem_op=1;op.sem_flg=0;
 			   semop(semid,&op,1);
 			   
@@ -99,19 +101,15 @@ int main (int argc, char ** argv) {
 		   }
 		}
    }
-   
-   if((pid = fork() == 0)){
-	   struct sigaction actions;
-	   int rc;
-	   sigset_t set;
-	   
+   pid = fork();
+   *(tabPid+atoi(argv[1])) = pid;
+   if(pid == 0){
 	   sigemptyset(&actions.sa_mask);
 	   actions.sa_flags = 0;
 	   actions.sa_handler = handler;
 	   rc = sigaction(SIGUSR1,&actions,NULL);
-	   
 	   while(1){
-		   
+		   //printf("PID f : %d\n",getpid());
 		   sigemptyset(&set);
 		   sigaddset(&set,SIGUSR1);
 		   sigprocmask(SIG_SETMASK, &set, NULL);
@@ -120,7 +118,7 @@ int main (int argc, char ** argv) {
 		   semop(semid,&op,1);
 		   
 		   if((sem=(int *)shmat(shmid,0,0)) == (int*)-1){
-			   fprintf(stderr, "Probleme sur shmat\n");
+			   fprintf(stderr, "Probleme sur shmatIci\n");
 			   exit(8);
 		   }
 		   
@@ -133,30 +131,35 @@ int main (int argc, char ** argv) {
 		   sigemptyset(&set);
 		   sigprocmask(SIG_SETMASK, &set, NULL);
 	   }
-   }
-   
-   getchar();
-   
-   for(i=0; i<atoi(argv[1]);i++) kill(pid,SIGUSR1);
-   
-   printf("Debug\n");
-   
-   for(i=0; i<atoi(argv[1]); i++){
-	   op.sem_num=0;op.sem_op=-1;op.sem_flg=0;
-	   semop(semid,&op,1);
-	   
-	   if((sem=(int *)shmat(shmid,0,0)) == (int*)-1){
-		   fprintf(stderr, "Probleme sur shmat\n");
-		   exit(10);
-	   }
-	   
-	   printf("%d ",*(sem+i));
-	   
-	   op.sem_num=0;op.sem_op=1;op.sem_flg=0;
-	   semop(semid,&op,1);
-   }
-   printf("\n");
-   
-   semctl(semid,0,IPC_RMID,0);
-   exit(0);
+	}
+	
+	getchar();
+
+	//printf("\r");
+	for(i=0; i<atoi(argv[1])+1;i++){
+		printf("Kill %d\n",tabPid[i]);
+		kill(tabPid[i],SIGUSR1);
+		wait(NULL);
+		sleep(1);
+	}
+	/*kill(tabPid[i+1],SIGUSR1);
+	wait(NULL);*/
+	
+	op.sem_num=0;op.sem_op=-1;op.sem_flg=0;
+	semop(semid,&op,1);
+	
+	if((sem=(int *)shmat(shmid,0,0)) == (int*)-1){
+		 fprintf(stderr, "Probleme sur shmat\n");
+		 exit(10);
+	 }
+	 
+	 //Blabla
+	 
+	 op.sem_num=0;op.sem_op=1;op.sem_flg=0;
+	 semop(semid,&op,1);
+	 
+	 printf("Destruction sémaphore\n");
+	 semctl(semid,0,IPC_RMID,0);
+	 
+    exit(0);
 }
