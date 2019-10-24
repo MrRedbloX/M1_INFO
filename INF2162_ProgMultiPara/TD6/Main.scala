7a40d08529137e06eab00b734f9cf02d4eb115f6
MrRedbloX <charles.cogoluegnes@gmail.com>
2019-10-24 18:58:18 +0200

    INF2162_ProgMultiPara TD6 END ?

--- INF2162_ProgMultiPara/TD6/Main.scala
+++ INF2162_ProgMultiPara/TD6/Main.scala
@@ -1,7 +1,11 @@
 object Main{
   def main(args : Array[String]) : Unit ={
     val euler = new CavalierEuler(6)
+    val timeStart = System.currentTimeMillis()
     euler.controller(0,0)
+    val timeEnd = System.currentTimeMillis()
+    val time : Double = ((timeEnd-timeStart).asInstanceOf[Double])/1000
+    println(s"Time : $time s")
     println(euler)
   }
 }
--- INF2162_ProgMultiPara/TD6/echec/CavalierEuler.scala
+++ INF2162_ProgMultiPara/TD6/echec/CavalierEuler.scala
@@ -3,6 +3,7 @@ class CavalierEuler(cote_ : Int){
   var cote = cote_
   var vue = new Echiquier[PieceCol](this.cote)
   var modele = Array.fill[Int](this.cote,this.cote)(0)
+  val heuristique = Array.fill(this.cote,this.cote)(0)
   def controller(xyDepart_ : Tuple2[Int,Int]) : Unit ={
     def trouveDeplacementsCavalier(xy_ : Tuple2[Int,Int]) : List[Tuple2[Int,Int]] ={
       var ret = List[Tuple2[Int,Int]]()
@@ -18,26 +19,25 @@ class CavalierEuler(cote_ : Int){
       ret = (xy_._1+2,xy_._2+1) :: ret
       ret = (xy_._1+2,xy_._2-1) :: ret
       ret = ret collect dansEchiquier
+      ret = ret.sortBy(e => this.heuristique(e._1)(e._2))
       ret
     }
     def trouvePositions(xy_ : Tuple2[Int,Int], etape_ : Int) : Boolean ={
       var ret : Boolean = false
       var pasDejaPassePar : PartialFunction[Tuple2[Int,Int],Tuple2[Int,Int]] ={
-        case e if(this.modele(e._1)(e._2) < etape_) => e
+        case e if(this.modele(e._1)(e._2) == 0) => e
       }
-      var listDeplacements = trouveDeplacementsCavalier(xy_) collect pasDejaPassePar
-      if(listDeplacements.isEmpty){
-        if(this.modele.flatten.contains(0)) ret = true
-      }
-      else{
-        breakable{
-          for(e <- listDeplacements){
-            this.modele(e._1)(e._2) = this.modele(e._1)(e._2) + 1
-            ret = trouvePositions(e, etape_ + 1)
-            if(ret) break
-          }
+      var listDeplacements = trouveDeplacementsCavalier(xy_)
+      this.heuristique(xy_._1)(xy_._2) = listDeplacements.size
+      listDeplacements = listDeplacements collect pasDejaPassePar
+      breakable{
+        for(e <- listDeplacements){
+          this.modele(e._1)(e._2) = etape_ + 1
+          if(trouvePositions(e,etape_ + 1)) break
+          else this.modele(e._1)(e._2) = 0
         }
       }
+      if(!this.modele.flatten.contains(0)) ret = true
       ret
     }
     def synchroniseVueAuModele() : Unit ={
@@ -47,6 +47,7 @@ class CavalierEuler(cote_ : Int){
     if(trouvePositions(xyDepart_,1)) println("Une solution a été trouvée.")
     else println("Pas de solution au problème.")
     synchroniseVueAuModele()
+    this.heuristique foreach {e => e foreach print _; println}
   }
   override def toString() : String = this.vue.toString
 
