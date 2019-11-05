ccc3515fe72f859adabd2b062ed1a33fc672749a
MrRedbloX <charles.cogoluegnes@gmail.com>
2019-11-05 13:48:47 +0100

    INF2162_ProgMultiPara TD6 END

--- INF2162_ProgMultiPara/TD6/Main.scala
+++ INF2162_ProgMultiPara/TD6/Main.scala
@@ -1,11 +1,12 @@
 object Main{
   def main(args : Array[String]) : Unit ={
-    val euler = new CavalierEuler(10)
+    var input = scala.io.StdIn.readLine("Cote de l'echiquer ? ").toInt
+    val euler = new CavalierEuler(input)
     val timeStart = System.currentTimeMillis()
     euler.controller(0,0)
     val timeEnd = System.currentTimeMillis()
     val time : Double = ((timeEnd-timeStart).asInstanceOf[Double])/1000
-    println(s"Time : $time s")
+    println(s"Temps : $time s")
     println(euler)
   }
 }
--- INF2162_ProgMultiPara/TD6/echec/CavalierEuler.scala
+++ INF2162_ProgMultiPara/TD6/echec/CavalierEuler.scala
@@ -4,14 +4,13 @@ class CavalierEuler(cote_ : Int){
   var vue = new Echiquier[PieceCol](this.cote)
   var modele = Array.fill[Int](this.cote,this.cote)(0)
   def controller(xyDepart_ : Tuple2[Int,Int]) : Unit ={
-    var heuristique = Array.fill(this.cote,this.cote)(0)
-    var pasDejaPassePar : PartialFunction[Tuple2[Int,Int],Tuple2[Int,Int]] ={
-      case e if(this.modele(e._1)(e._2) == 0) => e
-    }
     var dansEchiquier : PartialFunction[Tuple2[Int,Int],Tuple2[Int,Int]] ={
       case e if (e._1 >= 0 && e._1 < this.cote && e._2 >= 0 && e._2 < this.cote) => e
     }
-    def trouveDeplacementsCavalier(xy_ : Tuple2[Int,Int]) : List[Tuple2[Int,Int]] ={
+    var pasDejaPassePar : PartialFunction[Tuple2[Int,Int],Tuple2[Int,Int]] ={
+      case e if(this.modele(e._1)(e._2) == 0) => e
+    }
+    def deplacementsCavalier (xy_ : Tuple2[Int,Int]) : List[Tuple2[Int,Int]] ={
       var ret = List[Tuple2[Int,Int]]()
       ret = (xy_._1-2,xy_._2+1) :: ret
       ret = (xy_._1-2,xy_._2-1) :: ret
@@ -21,33 +20,39 @@ class CavalierEuler(cote_ : Int){
       ret = (xy_._1+1,xy_._2-2) :: ret
       ret = (xy_._1+2,xy_._2+1) :: ret
       ret = (xy_._1+2,xy_._2-1) :: ret
-      ret = ret collect dansEchiquier sortBy(e => heuristique(e._1)(e._2))
-      ret
+      ret collect dansEchiquier collect pasDejaPassePar
     }
+    var heuristique = Array.fill(this.cote,this.cote)(0)
+    heuristique = Array.tabulate(this.cote,this.cote)((x,y) => deplacementsCavalier((x,y)).size)
+    def trouveDeplacementsCavalier(xy_ : Tuple2[Int,Int]) : List[Tuple2[Int,Int]] = deplacementsCavalier(xy_) sortBy(e => heuristique(e._1)(e._2))
     def trouvePositions(xy_ : Tuple2[Int,Int], etape_ : Int) : Boolean ={
-      breakable{
-        for(e <- trouveDeplacementsCavalier(xy_) collect pasDejaPassePar){
-          this.modele(e._1)(e._2) = etape_ + 1
-          heuristique(e._1)(e._2) -= 1
-          if(trouvePositions(e,etape_ + 1)) break
-          else {
-            this.modele(e._1)(e._2) = 0
-            heuristique(e._1)(e._2) += 1
-          }
+      var ret : Boolean = true
+      heuristique = Array.tabulate(this.cote,this.cote)((x,y) => deplacementsCavalier((x,y)).size)
+      var deplacements = trouveDeplacementsCavalier(xy_)
+      this.modele(xy_._1)(xy_._2) = etape_
+      if(deplacements.size > 0){
+        ret = false
+        var i = 0
+        while(i < deplacements.size && !ret){
+          ret = trouvePositions(deplacements(i), etape_ + 1)
+          i += 1
         }
       }
-      if(!this.modele.flatten.contains(0)) true
-      else false
+      else{
+        if(!this.modele.flatten.contains(0)) ret = true
+        else ret = false
+      }
+      if(!ret) this.modele(xy_._1)(xy_._2) = 0
+      ret
     }
     def synchroniseVueAuModele() : Unit ={
       for(i <- 0 to this.cote-1; j <- 0 to this.cote-1) vue((i,j)) = Some(PieceCol(this.modele(i)(j).toString))
     }
-    heuristique = Array.tabulate(this.cote,this.cote)((x,y) => trouveDeplacementsCavalier((x,y)).size)
     this.modele(xyDepart_._1)(xyDepart_._2) = 1
     if(trouvePositions(xyDepart_,1)) println("Une solution a été trouvée.")
     else println("Pas de solution au problème.")
     synchroniseVueAuModele()
-    //this.heuristique foreach {e => e foreach print _; println}
+    //heuristique foreach {e => e foreach print _; println}
   }
   override def toString() : String = this.vue.toString
 }
