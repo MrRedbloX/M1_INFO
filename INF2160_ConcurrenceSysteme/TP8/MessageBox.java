4e2ca3251423da671a898103f1b1b9bb860b28b4
MrRedbloX <charles.cogoluegnes@gmail.com>
2019-11-28 14:39:43 +0100

    INF2160_ConcurrenceSysteme TP8 END

--- INF2160_ConcurrenceSysteme/TP8/MessageBox.java
+++ INF2160_ConcurrenceSysteme/TP8/MessageBox.java
@@ -9,8 +9,8 @@ import messagebox.Message;
  *  pour la communication de threads
  */
 public class MessageBox {
-	private LinkedList<Message> tampon;
-	private HashMap<Thread,Filter> blockThreads;
+	private LinkedList<Message> tampon; //La liste des messages
+	private HashMap<Thread,Filter> blockThreads; //La map des threads qui attendent un message d'un autre thread
 
   /**
    * constructeur
@@ -29,9 +29,9 @@ public class MessageBox {
 		   this.tampon.add(mes);
 			 synchronized(this.blockThreads){
 			   for(Thread t : this.blockThreads.keySet()) {
-				   if(this.blockThreads.get(t).isGood(mes)){
+				   if(this.blockThreads.get(t).isGood(mes)){ //Si l'éméteur du message correspond au filtre
 						 synchronized(t){
-							 t.notify();
+							 t.notify(); //On réveil le thread en wait
 							 this.blockThreads.remove(t);
 						 }
 						 break;
@@ -49,12 +49,12 @@ public class MessageBox {
    **/
    public Message receive(Filter f){
 		 Message ret = tryReceive(f);
-	   while(ret == null){
+	   while(ret == null){ //Si aucun message n'est trouvé
 			 synchronized(Thread.currentThread()){
 				 try{
-					 blockThreads.put(Thread.currentThread(),f);
-					 Thread.currentThread().wait();
-					 ret = tryReceive(f);
+					 blockThreads.put(Thread.currentThread(),f); //On ajoute le thread dans la liste des bloqués
+					 Thread.currentThread().wait(); //On l'endort
+					 ret = tryReceive(f); //Lorsqu'il est réveilé, il va donc chercher le fameux message
 				 }
 			 	 catch(InterruptedException ie){
 					 ie.printStackTrace();
@@ -65,8 +65,7 @@ public class MessageBox {
    }
 
  /**
-   * test si la MessageBox contient un message qui
-   *         repond a un fitre donne
+   * test si la MessageBox contient un message qui repond a un fitre donne
    *@param f filtre a appliquer (dont l'emt peut être éventuellement null)
    *@return Message  en cas de succes (null sinon)
    **/
@@ -74,9 +73,9 @@ public class MessageBox {
 		 Message ret = null;
 		 synchronized(this.tampon){
 		   for(Message m : this.tampon){
-			   if(f.isGood(m)){
-					  ret =  m;
-						this.tampon.remove(m);
+			   if(f.isGood(m)){ //Si le message correspond au filtre
+					  ret = m; //On va le retouner
+						this.tampon.remove(m); //Et on le supprime de la liste
 						break;
 					}
 		   }
