dbced79d500f714505f9998d40e354f84051cfb3
MrRedbloX <charles.cogoluegnes@gmail.com>
2019-12-01 18:03:45 +0100

    INF2162_ProgMultiPara T10 v1 END

--- INF2162_ProgMultiPara/TD10/Perceptron.scala
+++ INF2162_ProgMultiPara/TD10/Perceptron.scala
@@ -1,6 +1,7 @@
 import scala.{Array => $}
 import scala.util.Random.{shuffle => shu}
 import scala.math.{abs => abs}
+import scala.math.{exp => e}
 class Perceptron(couches_ : $[Int]){
   var oI = (0 until couches_.length).map(i => new $[Double](couches_(i))).toArray
   var iI = (0 until couches_.length).map(i => new $[Double](couches_(i))).toArray
@@ -9,41 +10,53 @@ class Perceptron(couches_ : $[Int]){
   def apply(in_ : $[Double]) : $[Double] = {
     require(in_.length == couches_(0), "Both vectors need to have to same length")
     for(i <- 0 until couches_(0)){ //On remplie les entrées
-      iI(0)(i) = in_(i)
-      oI(0)(i) = Perceptron.f(iI(0)(i))
+      this.iI(0)(i) = in_(i)
+      this.oI(0)(i) = Perceptron.f(this.iI(0)(i))
     }
     for(i <- 1 until couches_.length){ //On propage l'activation
       for(j <- 0 until couches_(i)){
-        iI(i)(j) = Perceptron.prod(oI(i-1), poids(i-1)(j)) //Les inputs sont décalés de 1 par rapport aux autres
-        oI(i)(j) = Perceptron.f(iI(i)(j))
+        this.iI(i)(j) = Perceptron.prod(this.oI(i-1), this.poids(i-1)(j)) //Les inputs sont décalés de 1 par rapport au output et aux poids
+        this.oI(i)(j) = Perceptron.f(this.iI(i)(j))
       }
     }
-    oI(couches_.length-1)
+    this.oI.last
   }
-  def retroPropag(observe_ : $[Double], souhaite_ : $[Double], pas_ : Double = 0.1) : Unit = {
-    var cS = couches_.length - 1 //Couche de sortie
-    for(i <- 1 until couches_.length){
-      for(j <- 0 until couches_(i)){
-        if(i == cS) dI(i)(j) = 2 * (observe_(j)-souhaite_(j)) * Perceptron.fp(iI(i)(j)) //Le cas particulier
-        else{
-          var sum = 0.0
-          for(h <- 0 until couches_(i+1)){
-            sum = sum + (dI(i+1)(h) * poids(i)(h)(j)) //La somme des h
-          }
-          dI(i)(j) = sum * Perceptron.fp(iI(i)(j))
-        }
-        for(k <- couches_(i-1) - 1 to 0 by -1){
-          poids(i-1)(j)(k) = poids(i-1)(j)(k) - (pas_ * dI(i)(j) * oI(i-1)(k)) //La correction des poids
+  def apprentissage(data_ : List[Tuple2[X,Y]], tolerance_ : Double = 0.05, nbItMax_ : Int = 50000) : Unit = {
+    def retroPropag(observe_ : $[Double], souhaite_ : $[Double], pas_ : Double = 0.1) : Unit = {
+      //Traitement de la première couche
+      for(j <- couches_(couches_.length-1)-1 to 0 by -1){
+        this.dI(couches_.length-1)(j) = 2 * (observe_(j)-souhaite_(j)) * Perceptron.fp(this.iI(couches_.length-1)(j)) //Le cas particulier de la couche de sortie
+        for(k <- couches_(couches_.length-2) - 1 to 0 by -1) this.poids(couches_.length-2)(j)(k) = this.poids(couches_.length-2)(j)(k) - (pas_ * this.dI(couches_.length-1)(j) * this.oI(couches_.length-2)(k)) //La correction des poids
+      }
+      //Traitement des couches suivantes
+      for(i <- couches_.length-2 to 1 by -1){
+        for(j <- couches_(i)-1 to 0 by -1){
+          for(h <- couches_(i+1)-1 to j by -1) this.dI(i)(j) = this.dI(i)(j) + (this.dI(i+1)(h) * this.poids(i)(h)(j) * Perceptron.fp(this.iI(i)(j))) //La somme des h
+          for(k <- couches_(i-1) - 1 to 0 by -1) this.poids(i-1)(j)(k) = this.poids(i-1)(j)(k) - (pas_ * this.dI(i)(j) * this.oI(i-1)(k)) //La correction des poids
         }
       }
     }
+    def erreur(ex_ : List[Tuple2[X,Y]]) : Double = ex_.map{ case (X(entree), Y(sortieSouhaitee)) => Perceptron.errQuad(sortieSouhaitee, this(entree))}.sum
+    def apprendreUneFois(ex_ : List[Tuple2[X,Y]]) : Unit = shu(ex_).foreach(e => retroPropag(this(e._1.x_), e._2.y_))
+    var nbIt = 0
+    var err = 1.0
+    println("Start")
+    while(err > tolerance_ && nbIt < nbItMax_){
+      err = abs(erreur(data_))
+      apprendreUneFois(data_)
+      nbIt = nbIt + 1
+      print(s"\rErreur : $err")
+    }
+    println(s"\nDone, nbIt : $nbIt")
   }
-  def erreur(ex_ : List[Tuple2[X,Y]]) : Double = ex_.map{ case (X(entree), Y(sortieSouhaitee)) => Perceptron.errQuad(sortieSouhaitee, this(entree))}.sum
-  def apprendreUneFois(ex_ : List[Tuple2[X,Y]]) : Unit = shu(ex_).foreach(e => retroPropag(this(e._1.x_), e._2.y_))
 }
 object Perceptron{
-  def f(x_ : Double) : Double = 1/(1+Math.exp(-x_))
-  def fp(x_ : Double) : Double = f(x_)*(1-f(x_))
+  def f(x_ : Double) : Double = fTanHyp(x_)
+  def fp(x_ : Double) : Double = fpTanHyp(x_)
+  def fSig(x_ : Double) : Double = 1/(1+e(-x_))
+  def fpSig(x_ : Double) : Double = fSig(x_)*(1-fSig(x_))
+  def fTanHyp(x_ : Double) : Double = (e(x_) - e(-x_)) / (e(x_) + e(-x_))
+  def fpTanHyp(x_ : Double) : Double = (1 + fTanHyp(x_)) * (1 - fTanHyp(x_))
   def prod(p1_ : $[Double], p2_ : $[Double]) : Double = {
     require(p1_.length == p2_.length, "Both vectors need to have to same length")
     p1_.zip(p2_).map{case (x,y) => x*y}.sum
@@ -54,21 +67,12 @@ object Perceptron{
   }
   def apply(couches_ : Int*) : Perceptron = new Perceptron(couches_.toArray)
   def main(args : $[String]) : Unit = {
-/*    val myP = Perceptron(3,1)
-    println(myP($[Double](0,0,1)).toList)*/
-    val tolerance = 0.05
-    val dataAnd = List[Tuple2[X,Y]]((X($[Double](0,0,1)),Y($[Double](0))),(X($[Double](0,1,1)),Y($[Double](0))),(X($[Double](1,0,1)),Y($[Double](0))),(X($[Double](1,1,1)),Y($[Double](1))))
+    val tolerance = 0.01
+    val dataAnd = List[Tuple2[X,Y]]((X($[Double](0,0,1)),Y($[Double](-1))),(X($[Double](0,1,1)),Y($[Double](-1))),(X($[Double](1,0,1)),Y($[Double](-1))),(X($[Double](1,1,1)),Y($[Double](1))))
+    val dataOr = List[Tuple2[X,Y]]((X($[Double](0,0,1)),Y($[Double](-1))),(X($[Double](0,1,1)),Y($[Double](1))),(X($[Double](1,0,1)),Y($[Double](1))),(X($[Double](1,1,1)),Y($[Double](1))))
+    val dataXOr = List[Tuple2[X,Y]]((X($[Double](0,0,1)),Y($[Double](-1))),(X($[Double](0,1,1)),Y($[Double](1))),(X($[Double](1,0,1)),Y($[Double](1))),(X($[Double](1,1,1)),Y($[Double](-1))))
     val perceptron = Perceptron(3,1)
-    var nbIt = 0
-    var err = 1.0
-    println("Start")
-    while(err > tolerance){
-      err = abs(perceptron.erreur(dataAnd))
-      perceptron.apprendreUneFois(dataAnd)
-      nbIt = nbIt + 1
-      println(err)
-    }
-    println(s"Done, nbIt : $nbIt")
+    perceptron.apprentissage(dataOr, 0.0001, 200000)
     println(perceptron($[Double](0,0,1)).toList)
     println(perceptron($[Double](0,1,1)).toList)
     println(perceptron($[Double](1,0,1)).toList)
